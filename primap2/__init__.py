6a74cfef6678a1280ae0947f4356baa8ad0aa59a
Mika Pflüger <mika.pflueger@pik-potsdam.de>
2021-02-01 18:48:06 +0100

    Add __all__.

--- primap2/__init__.py
+++ primap2/__init__.py
@@ -4,9 +4,10 @@ __author__ = """Mika Pflüger"""
 __email__ = "mika.pflueger@pik-potsdam.de"
 __version__ = "0.2.1"
 
-import pint
 import pint_xarray
 
 from . import accessors
 from ._data_format import open_dataset
 from ._units import ureg
+
+__all__ = ["accessors", "open_dataset", "ureg"]
