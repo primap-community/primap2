c7852e25baf5577d047232ee0cb6fd19cac84046
Mika Pflüger <mika.pflueger@climate-resource.com>
2024-03-15 14:43:32 +0100

    feat: compose_timeseries: add source information in a second result array

--- primap2/csg/_compose.py
+++ primap2/csg/_compose.py
@@ -2,6 +2,7 @@
 
 import typing
 
+import numpy as np
 import xarray as xr
 from attrs import define
 from loguru import logger
@@ -32,17 +33,44 @@ class PriorityDefinition:
 
 class FillingStrategyModel(typing.Protocol):
     """
-    Fill missing data in a dataset using another dataset.
+    Fill missing data in a timeseries using another timeseries.
     """
 
     type: str
 
-    def fill(self, ts: xr.DataArray, fill_ts: xr.DataArray) -> xr.DataArray:
+    def fill(
+        self,
+        *,
+        ts: xr.DataArray,
+        fill_ts: xr.DataArray,
+        fill_ts_repr: str,
+        sources_ts: xr.DataArray,
+    ) -> tuple[xr.DataArray, xr.DataArray]:
         """Fill gaps in ts using data from the fill_ts.
 
-        ts and fill_ts will not be modified.
+        Parameters
+        ----------
+        ts
+            Base timeseries. Missing data (NaNs) in this timeseries will be filled.
+            This function must not modify the data in ts.
+        fill_ts
+            Fill timeseries. Data from this timeseries will be used (possibly after
+            modification) to fill missing data in the base timeseries.
+            This function must not modify the data in fill_ts.
+        fill_ts_repr
+            String representation of fill_ts. Human-readable short representation of
+            the fill_ts (e.g. the source).
+        sources_ts
+            Source information timeseries. String representation of the sources
+            for the data in ts, with the same shape as ts.
+            This function must not modify the data in sources_ts.
 
-        The filling may only partly fill missing data.
+        Returns
+        -------
+            filled_ts, filled_sources_ts. filled_ts contains the result, where missing
+            data in ts is (partly) filled using information from fill_ts.
+            filled_sources_ts contains a string representation of the derivation for
+            each data point.
         """
         ...
 
@@ -56,9 +84,50 @@ class SubstitutionStrategy:
 
     type = "substitution"
 
-    def fill(self, ts: xr.DataArray, fill_ts: xr.DataArray) -> xr.DataArray:
-        """Fill gaps in ts using data from the fill_ts."""
-        return xr.core.ops.fillna(ts, fill_ts, join="exact")
+    def fill(
+        self,
+        *,
+        ts: xr.DataArray,
+        fill_ts: xr.DataArray,
+        fill_ts_repr: str,
+        sources_ts: xr.DataArray,
+    ) -> tuple[xr.DataArray, xr.DataArray]:
+        """Fill gaps in ts using data from the fill_ts.
+
+        Parameters
+        ----------
+        ts
+            Base timeseries. Missing data (NaNs) in this timeseries will be filled.
+            This function does not modify the data in ts.
+        fill_ts
+            Fill timeseries. Data from this timeseries will be used (possibly after
+            modification) to fill missing data in the base timeseries.
+            This function does not modify the data in fill_ts.
+        fill_ts_repr
+            String representation of fill_ts. Human-readable short representation of
+            the fill_ts (e.g. the source).
+        sources_ts
+            Source information timeseries. String representation of the sources
+            for the data in ts, with the same shape as ts.
+            This function does not modify the data in sources_ts.
+
+        Returns
+        -------
+            filled_ts, filled_sources_ts. filled_ts contains the result, where missing
+            data in ts is (partly) filled using unmodified data from fill_ts.
+            filled_sources_ts is the same as sources_ts, but with fill_ts_repr at
+            coordinates which were filled in filled_ts.
+        """
+        # data
+        filled_ts = xr.core.ops.fillna(ts, fill_ts, join="exact")
+        # source info
+        fill_sources_ts = xr.full_like(fill_ts, fill_ts_repr, dtype=object)
+        fill_sources_ts[fill_ts.isnull()] = np.nan
+        filled_sources_ts = xr.core.ops.fillna(
+            sources_ts, fill_sources_ts, join="exact"
+        )
+
+        return filled_ts, filled_sources_ts
 
 
 @define
@@ -103,13 +172,48 @@ class StrategyDefinition:
         raise KeyError(f"No matching strategy found for {fill_ts.coords}")
 
 
+def priority_coordinates_repr(
+    *, fill_ts: xr.DataArray, priority_dimensions: list[str]
+) -> str:
+    """Reduce the priority coordinates to a short string representation"""
+    priority_coordinates: dict[str, str] = {
+        k: fill_ts[k].item() for k in priority_dimensions
+    }
+    if len(priority_coordinates) == 1:
+        # only one priority dimension, just output the value because it is clear what is
+        # meant
+        return repr(next(iter(priority_coordinates.values())))
+    return repr(priority_coordinates)
+
+
 def compose_timeseries(
     *,
     input_data: xr.DataArray,
     priority_definition: PriorityDefinition,
     strategy_definition: StrategyDefinition,
-) -> xr.DataArray:
-    """TODO: logging? source tracebility info?"""
+) -> tuple[xr.DataArray, xr.DataArray]:
+    """
+    Compute a single timeseries from given input data, priorities, and strategies.
+
+    Parameters
+    ----------
+    input_data
+        The input data which has dimensions time plus the priority dimensions. The
+        fixed coordinates are supplied as zero-dimensional coordinates.
+    priority_definition
+        The definition of priorities within input_data. Each priority selects a single
+        timeseries (i.e. an array with only time as the dimension), so has to specify
+        values for all priority dimensions.
+    strategy_definition
+        The definition of strategies for timeseries in input_data.
+
+    Returns
+    -------
+        result_ts, result_sources_ts. In result_ts is the numerical result, in
+        result_sources_ts is the string representation of the derivation of each data
+        point. Both are a single timeseries with the same fixed coordinates as
+        input_data and the same time dimension like input_data.
+    """
     context_logger = logger.bind(
         fixed_coordinates={k: v for k, v in input_data.coords.items() if v.shape == ()},
         priority_coordinates={
@@ -120,6 +224,7 @@ def compose_timeseries(
     )
 
     result_ts: None | xr.DataArray = None
+    result_sources_ts: None | xr.DataArray = None
     for selector in priority_definition.priorities:
         try:
             fill_ts = input_data.loc[selector]
@@ -127,15 +232,29 @@ def compose_timeseries(
             context_logger.debug(f"{selector=} matched no input_data, skipping.")
             continue
 
-        if result_ts is None:
+        fill_ts_repr = priority_coordinates_repr(
+            fill_ts=fill_ts, priority_dimensions=priority_definition.dimensions
+        )
+
+        if result_ts is None or result_sources_ts is None:
             context_logger.debug(
-                f"{ {k: fill_ts[k].item() for k in priority_definition.dimensions} } is"
-                f" the highest-priority source, using as the basis to fill."
+                f"{fill_ts_repr} is the highest-priority source, using as the "
+                f"basis to fill."
             )
             result_ts = fill_ts
+            result_sources_ts = xr.full_like(
+                fill_ts, fill_value=fill_ts_repr, dtype=object
+            )
+            result_sources_ts[result_ts.isnull()] = np.nan
         else:
+            context_logger.debug(f"Filling with {fill_ts_repr} now.")
             strategy = strategy_definition.find_strategy(fill_ts)
-            result_ts = strategy.fill(result_ts, fill_ts)
+            result_ts, result_sources_ts = strategy.fill(
+                ts=result_ts,
+                fill_ts=fill_ts,
+                fill_ts_repr=fill_ts_repr,
+                sources_ts=result_sources_ts,
+            )
 
         if not result_ts.isnull().any():
             context_logger.debug("No NaNs remaining, skipping the rest of the sources.")
@@ -146,4 +265,4 @@ def compose_timeseries(
             f"No selector matched for \n{input_data.coords}\n{priority_definition=}"
         )
 
-    return result_ts
+    return result_ts, result_sources_ts
--- primap2/tests/csg/test_compose.py
+++ primap2/tests/csg/test_compose.py
@@ -1,5 +1,7 @@
 """Tests for csg/test_compose.py"""
 
+from collections.abc import Sequence
+
 import numpy as np
 import pandas as pd
 import pytest
@@ -8,12 +10,30 @@ import xarray as xr
 import primap2.csg._compose
 
 
-def test_timeseries_selector():
-    time = pd.date_range("1850-01-01", "2022-01-01", freq="YS")
-    anp = np.linspace(0.0, 1.0, len(time))
-    da = xr.DataArray(
-        anp, dims=["time"], coords={"time": time, "source": "A", "category": "1.A"}
+def get_single_ts(
+    *,
+    time: pd.DatetimeIndex | None = None,
+    data: np.ndarray | None = None,
+    dims: Sequence[str] | None = None,
+    coords: dict[str, str | Sequence[str]] | None = None,
+) -> xr.DataArray:
+    if time is None:
+        time = pd.date_range("1850-01-01", "2022-01-01", freq="YS")
+    if dims is None:
+        dims = []
+    if data is None:
+        data = np.linspace(0.0, 1.0, len(time))
+    if coords is None:
+        coords = {}
+    return xr.DataArray(
+        data,
+        dims=["time", *dims],
+        coords={"time": time, **coords},
     )
+
+
+def test_timeseries_selector():
+    da = get_single_ts(coords={"source": "A", "category": "1.A"})
 
     assert primap2.csg._compose.TimeseriesSelector({"source": "A"}).match(da)
     assert not primap2.csg._compose.TimeseriesSelector({"source": "B"}).match(da)
@@ -26,11 +46,7 @@ def test_timeseries_selector():
 
 
 def test_strategy_definition():
-    time = pd.date_range("1850-01-01", "2022-01-01", freq="YS")
-    anp = np.linspace(0.0, 1.0, len(time))
-    da = xr.DataArray(
-        anp, dims=["time"], coords={"time": time, "source": "A", "category": "1.A"}
-    )
+    da = get_single_ts(coords={"source": "A", "category": "1.A"})
 
     ts = primap2.csg._compose.TimeseriesSelector
     assert (
@@ -73,23 +89,25 @@ def test_compose_timeseries_trivial():
 
     time = pd.date_range("1850-01-01", "2022-01-01", freq="YS")
     anp = np.linspace(0.0, 1.0, len(time))
-    bnp = np.linspace(1000.0, 2000.0, len(time))
     anp[0] = np.nan
     anp[1] = np.nan
+    da_a = get_single_ts(
+        coords={"source": "A", "category": "1", "area (ISO3)": "MEX"},
+        data=anp,
+        time=time,
+    )
+    bnp = np.linspace(1000.0, 2000.0, len(time))
     bnp[1] = np.nan
     bnp[2] = np.nan
-    input_data = xr.DataArray(
-        np.array([anp, bnp]).T,
-        dims=["time", "source"],
-        coords={
-            "source": ["A", "B"],
-            "time": time,
-            "category": "1",
-            "area (ISO3)": "MEX",
-        },
+    da_b = get_single_ts(
+        coords={"source": "B", "category": "1", "area (ISO3)": "MEX"},
+        data=bnp,
+        time=time,
     )
 
-    result = primap2.csg._compose.compose_timeseries(
+    input_data = xr.concat((da_a, da_b), dim="source", join="exact")
+
+    result_ts, result_sources_ts = primap2.csg._compose.compose_timeseries(
         input_data=input_data,
         priority_definition=priority_definition,
         strategy_definition=strategy_definition,
@@ -98,14 +116,40 @@ def test_compose_timeseries_trivial():
     enp = np.linspace(0.0, 1.0, len(time))  # same as A
     enp[0] = 1000.0  # filled from B
     enp[1] = np.nan  # not filled, NaN in A and B
-    expected = xr.DataArray(
-        enp,
-        dims=["time"],
-        coords={
-            "time": time,
-            "category": "1",
-            "area (ISO3)": "MEX",
-        },
+    expected_ts = get_single_ts(
+        data=enp, coords={"category": "1", "area (ISO3)": "MEX"}, time=time
     )
 
-    xr.testing.assert_identical(result, expected)
+    esnp = np.full_like(enp, "'A'", dtype=object)
+    esnp[0] = "'B'"  # filled from B
+    esnp[1] = np.nan  # not filled, NaN in A and B, so no source
+    expected_sources_ts = get_single_ts(
+        data=esnp, coords={"category": "1", "area (ISO3)": "MEX"}, time=time
+    )
+
+    xr.testing.assert_identical(result_ts, expected_ts)
+    xr.testing.assert_identical(result_sources_ts, expected_sources_ts)
+
+
+def test_priority_coordinates_repr():
+    assert (
+        primap2.csg._compose.priority_coordinates_repr(
+            fill_ts=get_single_ts(coords={"source": "A"}),
+            priority_dimensions=["source"],
+        )
+        == "'A'"
+    )
+    assert (
+        primap2.csg._compose.priority_coordinates_repr(
+            fill_ts=get_single_ts(coords={"source": "A", "scenario": "S"}),
+            priority_dimensions=["source", "scenario"],
+        )
+        == "{'source': 'A', 'scenario': 'S'}"
+    )
+    assert (
+        primap2.csg._compose.priority_coordinates_repr(
+            fill_ts=get_single_ts(coords={"source": "A", "scenario": "S"}),
+            priority_dimensions=["scenario"],
+        )
+        == "'S'"
+    )
