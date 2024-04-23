0dc294152061cb3f4301384afa7850a4a63f2942
Mika Pflüger <mika.pflueger@climate-resource.com>
2024-04-23 14:24:58 +0200

    test: add test for conformance with strategy protocol.

--- primap2/tests/csg/test_strategies.py
+++ primap2/tests/csg/test_strategies.py
@@ -1,7 +1,32 @@
 import numpy as np
+import pytest
+import xarray as xr
+import xarray.testing
 
 import primap2.csg
 from primap2.tests.csg.utils import get_single_ts
+
+
+@pytest.mark.parametrize(
+    "strategy", [primap2.csg.SubstitutionStrategy(), primap2.csg.NullStrategy()]
+)
+def test_strategies_conform(strategy):
+    """Test if strategies conform to the protocol for strategies."""
+    ts = get_single_ts(data=1.0)
+    ts[0] = np.nan
+    fill_ts = get_single_ts(data=2.0)
+    initial_ts = ts.copy(deep=True)
+    initial_fill_ts = fill_ts.copy(deep=True)
+
+    res, res_desc = strategy.fill(ts=ts, fill_ts=fill_ts, fill_ts_repr="B")
+
+    # ts and fill_ts must not be modified
+    xr.testing.assert_identical(ts, initial_ts)
+    xr.testing.assert_identical(fill_ts, initial_fill_ts)
+
+    assert isinstance(res, xr.DataArray)
+    assert isinstance(res_desc, list)
+    assert all(isinstance(step, primap2.ProcessingStepDescription) for step in res_desc)
 
 
 def test_substitution_strategy():
@@ -9,7 +34,7 @@ def test_substitution_strategy():
     ts[0] = np.nan
     fill_ts = get_single_ts(data=2.0)
 
-    (result_ts, result_descriptions) = primap2.csg.SubstitutionStrategy().fill(
+    result_ts, result_descriptions = primap2.csg.SubstitutionStrategy().fill(
         ts=ts, fill_ts=fill_ts, fill_ts_repr="B"
     )
     assert result_ts[0] == 2.0
